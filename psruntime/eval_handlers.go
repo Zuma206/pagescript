9843ac2d21a2ce844937ce82ba06494d229e6788
Zuma206 <zuma@zuma.eu.org>
2025-09-03 22:11:57 +0100

    Added element node eval handlers

--- psruntime/eval_handlers.go
+++ psruntime/eval_handlers.go
@@ -6,12 +6,14 @@ import (
 	"golang.org/x/net/html"
 )
 
-var evalNodeHandlers = &NodeHandlerRegistry{
-	nodeHandlers: NodeHandlerMap{
+var evalNodeHandlers = NewNodeHandlers(
+	NodeHandlerMap{
 		html.DocumentNode: evalChildren,
 		html.DoctypeNode:  evalDoctype,
+		html.ElementNode:  evalElement,
 	},
-}
+	ElementHandlerMap{},
+)
 
 func evalChildren(psc *PSContext, node *html.Node) error {
 	for child := range node.ChildNodes() {
@@ -25,4 +27,32 @@ func evalChildren(psc *PSContext, node *html.Node) error {
 func evalDoctype(psc *PSContext, node *html.Node) error {
 	_, err := fmt.Fprintf(psc.Output(), "<!DOCTYPE %s>", node.Data)
 	return err
+}
+
+func evalElement(psc *PSContext, node *html.Node) error {
+	if _, err := fmt.Fprintf(psc.Output(), "<%s", node.Data); err != nil {
+		return err
+	}
+	if err := evalAttrs(psc, node); err != nil {
+		return err
+	}
+	if _, err := fmt.Fprint(psc.Output(), ">"); err != nil {
+		return err
+	}
+	if err := evalChildren(psc, node); err != nil {
+		return err
+	}
+	if _, err := fmt.Fprintf(psc.Output(), "</%s>", node.Data); err != nil {
+		return err
+	}
+	return nil
+}
+
+func evalAttrs(psc *PSContext, node *html.Node) error {
+	for _, attr := range node.Attr {
+		if _, err := fmt.Fprintf(psc.Output(), " %s=\"%s\"", attr.Key, attr.Val); err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- psruntime/registry.go
+++ psruntime/registry.go
@@ -3,13 +3,15 @@ package psruntime
 import "golang.org/x/net/html"
 
 type NodeHandlerMap map[html.NodeType]NodeHandler
+type ElementHandlerMap map[string]NodeHandler
 
 type NodeHandlerRegistry struct {
-	nodeHandlers NodeHandlerMap
+	nodeHandlers    NodeHandlerMap
+	elementHandlers ElementHandlerMap
 }
 
-func NewNodeHandlers(handlers NodeHandlerMap) *NodeHandlerRegistry {
-	return &NodeHandlerRegistry{handlers}
+func NewNodeHandlers(nodeHandlers NodeHandlerMap, elementHandlers ElementHandlerMap) *NodeHandlerRegistry {
+	return &NodeHandlerRegistry{nodeHandlers, elementHandlers}
 }
 
 func (registry *NodeHandlerRegistry) SetNodeHandler(nodeType html.NodeType, handler NodeHandler) {
@@ -17,6 +19,25 @@ func (registry *NodeHandlerRegistry) SetNodeHandler(nodeType html.NodeType, hand
 }
 
 func (registry *NodeHandlerRegistry) Get(nodeType html.NodeType) (NodeHandler, bool) {
+	if nodeType == html.ElementNode {
+		return registry.handleElement, true
+	}
 	handler, ok := registry.nodeHandlers[nodeType]
 	return handler, ok
+}
+
+func (registry *NodeHandlerRegistry) handleElement(psc *PSContext, node *html.Node) error {
+	handler := registry.elementHandlers[node.Data]
+	if handler != nil {
+		return handler(psc, node)
+	}
+	defaultHandler := registry.nodeHandlers[html.ElementNode]
+	if defaultHandler != nil {
+		return defaultHandler(psc, node)
+	}
+	return nil
+}
+
+func (registry *NodeHandlerRegistry) SetElementHandler(elementType string, handler NodeHandler) {
+	registry.elementHandlers[elementType] = handler
 }

e8a4d7a4fa8888c6f26f48bbefe9d6dbbfff9cff
Zuma206 <zuma@zuma.eu.org>
2025-09-14 18:50:38 +0100

    Added WithInitialWorkers eventloop option

--- eventloop/constructor.go
+++ eventloop/constructor.go
@@ -3,6 +3,7 @@ package eventloop
 type EventloopOptions struct {
 	continuationQueueSize uint
 	taskQueueSize         uint
+	initialWorkers        uint
 }
 
 type EventloopOption func(options *EventloopOptions)
@@ -11,6 +12,7 @@ func newEventloopOptions(options []EventloopOption) *EventloopOptions {
 	optionsStruct := &EventloopOptions{
 		continuationQueueSize: 32,
 		taskQueueSize:         32,
+		initialWorkers:        0,
 	}
 	for _, option := range options {
 		option(optionsStruct)
@@ -20,10 +22,12 @@ func newEventloopOptions(options []EventloopOption) *EventloopOptions {
 
 func NewEventloop(options ...EventloopOption) *Eventloop {
 	opts := newEventloopOptions(options)
-	return &Eventloop{
+	eventloop := &Eventloop{
 		continuations: make(chan *Continuation, opts.continuationQueueSize),
 		tasks:         make(chan *Task, opts.continuationQueueSize),
 	}
+	eventloop.Workers(opts.initialWorkers)
+	return eventloop
 }
 
 func WithContinuationQueueSize(continuationQueueSize uint) EventloopOption {
@@ -35,5 +39,11 @@ func WithContinuationQueueSize(continuationQueueSize uint) EventloopOption {
 func WithTaskQueueSize(taskQueueSize uint) EventloopOption {
 	return func(options *EventloopOptions) {
 		options.taskQueueSize = taskQueueSize
+	}
+}
+
+func WithInitialWorkers(initialWorkers uint) EventloopOption {
+	return func(options *EventloopOptions) {
+		options.initialWorkers = initialWorkers
 	}
 }

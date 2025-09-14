e791ff829b0eff5a594bcd4357a7e437bbf36081
Zuma206 <zuma@zuma.eu.org>
2025-09-14 18:28:43 +0100

    Added go and continue eventloop methods

--- eventloop/eventloop.go
+++ eventloop/eventloop.go
@@ -4,3 +4,13 @@ type Eventloop struct {
 	continuations chan *Continuation
 	tasks         chan *Task
 }
+
+func (eventloop *Eventloop) Go(function TaskFunction) {
+	eventloop.tasks <- &Task{function}
+}
+
+func (eventloop *Eventloop) Continue(function ContinuationFunction) *Continuation {
+	continuation := NewContinuation(function)
+	eventloop.continuations <- continuation
+	return continuation
+}

693eb09f8e03a44c60df01bff81443be7a3ccc76
Zuma206 <zuma@zuma.eu.org>
2025-09-15 00:36:47 +0100

    Added eventloop task concurrency test

--- eventloop/eventloop_test.go
+++ eventloop/eventloop_test.go
@@ -34,3 +34,37 @@ func TestContinuationsRunInSerial(t *testing.T) {
 	wg.Wait()
 	loop.Stop()
 }
+
+const (
+	nTasks = 10
+)
+
+func TestTasksRunInParallel(t *testing.T) {
+	loop := NewEventloop(WithTaskQueueSize(nTasks))
+	expectedTime := nTasks * simulatedWorkLength
+	startTime := time.Now()
+	var wg sync.WaitGroup
+
+	for range nTasks {
+		wg.Add(1)
+		loop.Go(func() ContinuationFunction {
+			time.Sleep(simulatedWorkLength)
+			wg.Done()
+			return nil
+		})
+	}
+
+	go loop.Worker()
+	go loop.Worker()
+	wg.Wait()
+	loop.Stop()
+
+	tasksTime := time.Since(startTime)
+	if tasksTime >= expectedTime {
+		t.Errorf(
+			"tasks took %dms, which is more than/equal to the estimated serialised time (%dms)",
+			tasksTime.Milliseconds(),
+			expectedTime.Milliseconds(),
+		)
+	}
+}

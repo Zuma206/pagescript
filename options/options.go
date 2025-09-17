1caa3dadd10990f620f25f09987b1b7bb90aa98b
Zuma206 <zuma@zuma.eu.org>
2025-09-17 18:13:50 +0100

    Fixed options.New naming

--- options/options.go
+++ options/options.go
@@ -4,7 +4,7 @@ type Option[T any] func(target T) error
 type OptionConstructor[T any, V any] func(value V) Option[T]
 type OptionFunction[T any, V any] func(target T, value V) error
 
-func NewOptionConstructor[T any, V any](function OptionFunction[T, V]) OptionConstructor[T, V] {
+func New[T any, V any](function OptionFunction[T, V]) OptionConstructor[T, V] {
 	return func(value V) Option[T] {
 		return func(target T) error {
 			return function(target, value)

1bdc8b2940b8ecc1a7644fb30380845ecd7cf528
Zuma206 <zuma@zuma.eu.org>
2025-09-23 14:54:15 +0100

    Fixed error handling for id scan handler

--- stdlib/elements.go
+++ stdlib/elements.go
@@ -9,7 +9,9 @@ func OpenElements(psr *psruntime.PSRuntime) error {
 	elements := psr.Engine().NewObject()
 	psr.ScanHandlers().SetAttrHandler("id", func(_ *psruntime.PSContext, node *html.Node, id string) error {
 		element := psr.Engine().NewObject()
-		element.Set("type", node.Data)
+		if err := element.Set("type", node.Data); err != nil {
+			return err
+		}
 		return elements.Set(id, element)
 	})
 	return psr.Engine().Set("elements", elements)
